51ad037787c26085cb9eafee9a109644743dfdbc
swathisekar28 <121334996+swathisekar28@users.noreply.github.com>
2023-01-07 15:00:14 +0530

    Delete ex4 convert decimal to binary.py

--- ex4 convert decimal to binary.py
+++
@@ -1,8 +0,0 @@
-Python 3.5.3 (v3.5.3:1880cb95a742, Jan 16 2017, 15:51:26) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-== RESTART: C:/Users/TEMP.VCET.042/Desktop/ex4 covert decimal to binary.py ==
-enter a number:8
-binary equivalent is:
-1 0 0 0 
->>> 
